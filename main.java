8fa1e89545c8d21a4292366deaeadb430769e662
Adithyan <sadithyan403@gmail.com>
2023-05-02 17:38:58 +0530

    added the button and text area

--- main.java
+++ main.java
@@ -5,9 +5,15 @@ public class main extends JFrame{
 		setTitle("Email Stripper");
 		setSize(400,300);
 		setLayout(new BorderLayout());
-		JLabel label=new JLabel("Welcome \n Input the email in the text box");
+		JLabel label=new JLabel("            Welcome \n Input the email in the text box");
 
 		add(label,BorderLayout.NORTH);
+		JButton button=new JButton("Click TO Convert");
+		add(button,BorderLayout.SOUTH);
+		JTextField text=new JTextField(5);
+		add(text);
+		text.setPreferredSize(new Dimension(200, 30));
+		text.setBackground(Color.blue);
 		setVisible(true);
 
 	}
