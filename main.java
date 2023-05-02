06c3b50b37c023b6a2435b3f40ba4cd61aae3a66
Adithyan <sadithyan403@gmail.com>
2023-05-02 09:52:35 +0530

    created the main window

--- main.java
+++ main.java
@@ -1,5 +1,19 @@
-public class main{
+import javax.swing.*;
+import java.awt.*;
+public class main extends JFrame{
+	public main(){
+		setTitle("Email Stripper");
+		setSize(400,300);
+		setLayout(new BorderLayout());
+		JLabel label=new JLabel("Welcome \n Input the email in the text box");
+
+		add(label,BorderLayout.NORTH);
+		setVisible(true);
+
+	}
 	public static void main(String[] args) {
-	System.out.println("hello world");
-}
+		new main();
+	}
+
+
 }
